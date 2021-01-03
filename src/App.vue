1ffdc85ac0ddcf6bab44790ac2f479c2bfbdc271
Jhirroh <markryan_dalisay@yahoo.com>
2021-01-03 18:38:38 +0800

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <h4>Events For Good</h4>
   </div>
   <router-view />
 </template>
@@ -26,5 +27,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+h4 {
+  font-size: 20px;
 }
 </style>
